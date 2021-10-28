0b829e85602301ac687f624ad5e8f644b49b0e72
km1562 <wkm690772123@gmail.com>
2021-10-28 11:06:56 +0800

    soft link for dataset

---
+++ AdelaiDet/datasets/generate_soft_ln.sh
@@ -0,0 +1,5 @@
+ln -s /home/datasets/textGroup/Adelaide/CTW1500 /home/wengkangming/map_file/AdelaiDet2/AdelaiDet/datasets/CTW1500
+ln -s /home/datasets/textGroup/Adelaide/totaltext /home/wengkangming/map_file/AdelaiDet2/AdelaiDet/datasets/totaltext
+ln -s /home/datasets/textGroup/Adelaide/mlt2017 /home/wengkangming/map_file/AdelaiDet2/AdelaiDet/datasets/mlt2017
+ln -s /home/datasets/textGroup/Adelaide/syntext /home/wengkangming/map_file/AdelaiDet2/AdelaiDet/datasets/syntext
+ln -s /home/datasets/textGroup/Adelaide/evaluation /home/wengkangming/map_file/AdelaiDet2/AdelaiDet/datasets/evaluation
