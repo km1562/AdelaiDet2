0c4069d14f2db2c106dd63505bb04e98b66f781b
km1562 <wkm690772123@gmail.com>
2021-11-17 10:53:42 +0800

    Adelaide_备份

--- AdelaiDet/adet/modeling/backbone/__init__.py
+++ AdelaiDet/adet/modeling/backbone/__init__.py
@@ -3,3 +3,4 @@ from .vovnet import build_vovnet_fpn_backbone, build_vovnet_backbone
 from .dla import build_fcos_dla_fpn_backbone
 from .resnet_lpf import build_resnet_lpf_backbone
 from .bifpn import build_fcos_resnet_bifpn_backbone
+from .img_to_show_CAM import img_to_show_CAM
