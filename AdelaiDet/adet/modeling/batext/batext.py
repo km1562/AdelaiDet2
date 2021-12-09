fdfecbd620c66829830b585940906fe23037c189
km1562 <wkm690772123@gmail.com>
2021-12-09 21:40:51 +0800

    "CBAM+权重特征图的训练"

--- AdelaiDet/adet/modeling/batext/batext.py
+++ AdelaiDet/adet/modeling/batext/batext.py
@@ -15,6 +15,8 @@ __all__ = ["BAText"]
 
 INF = 100000000
 
+def swish(x):
+    return x * x.sigmoid()
 
 class Scale(nn.Module):
     def __init__(self, init_value=1.0):
@@ -151,7 +153,10 @@ class BAText(nn.Module):
         self.fcos_head = FCOSHead(cfg, [input_shape[f] for f in self.in_features])
         # self.predict_pre_nms_thresh = Predict_pre_nms_thresh(cfg)
 
-
+        self.weight_feature = nn.Parameter(
+            torch.ones(len(self.in_features), dtype=torch.float32,
+                       requires_grad=True)
+        )
 
     def forward_head(self, features, top_module=None):
         features = [features[f] for f in self.in_features]
@@ -174,7 +179,34 @@ class BAText(nn.Module):
         """
         features = [features[f] for f in self.in_features]
 
-        #TODO,这里就进行multi_fuse,+卷积softmax对他进行一个阈值的输出，并且融合的feature_fuse要传出去
+        features = [features[f] for f in self.in_features]
+
+        # generation weight 1
+        # weights = F.relu(self.__getattr__(self.name))
+        # norm_weights = weights / (weights.sum() + 0.0001)
+        # weights_features = []
+        # for i, feature in enumerate(features):
+        #     norm_weight = norm_weights[i]
+        #     weithg_feature = feature * norm_weight
+        #     weithg_feature = swish(weithg_feature)
+        #     weights_features.append(weithg_feature)
+        #
+        # features = weights_features
+
+        # generation weight 2
+        weights = F.relu(self.weight_feature)
+        norm_weights = weights / (weights.sum() + 0.0001)
+        # new_node = torch.stack(features, dim=-1)
+        # new_node = (norm_weights * new_node).sum(dim=-1)  #这里会变成一个节点！
+        # new_node = swish(new_node)
+        weights_features = []
+        for i, feature in enumerate(features):
+            norm_weight = norm_weights[i]
+            weithg_feature = feature * norm_weight
+            weithg_feature = swish(weithg_feature)
+            weights_features.append(weithg_feature)
+
+        features = weights_features
 
         locations = self.compute_locations(features)
         logits_pred, reg_pred, ctrness_pred, top_feats, bbox_towers = self.fcos_head(
