1f2ce417392c6787e623d80bfb6660f04a2a0bcd
km1562 <wkm690772123@gmail.com>
2021-12-10 15:07:27 +0800

    "CBAM+权重特征图的bifpn更新"

---
+++ AdelaiDet/adet/modeling/backbone/img_to_show_CAM.py
@@ -0,0 +1,1 @@
+img_to_show_CAM = None
---
+++ AdelaiDet/tools/CAM_CBAM_train_net.py
@@ -0,0 +1,312 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+"""
+Detection Training Script.
+
+This scripts reads a given config file and runs the training or evaluation.
+It is an entry point that is made to train standard models in detectron2.
+
+In order to let one script support training of many models,
+this script contains logic that are specific to these built-in models and therefore
+may not be suitable for your own project.
+For example, your research project perhaps only needs a single "evaluator".
+
+Therefore, we recommend you to use detectron2 as an library and take
+this file as an example of how to use the library.
+You may want to write your own script with your datasets and other customizations.
+"""
+
+import logging
+import os
+from collections import OrderedDict
+import torch
+from torch.nn.parallel import DistributedDataParallel
+
+import detectron2.utils.comm as comm
+from detectron2.data import MetadataCatalog, build_detection_train_loader
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from detectron2.utils.events import EventStorage
+from detectron2.evaluation import (
+    COCOEvaluator,
+    COCOPanopticEvaluator,
+    DatasetEvaluators,
+    LVISEvaluator,
+    PascalVOCDetectionEvaluator,
+    SemSegEvaluator,
+    verify_results,
+)
+from detectron2.modeling import GeneralizedRCNNWithTTA
+from detectron2.utils.logger import setup_logger
+from adet.modeling.roi_heads.seg_text_head import Seg_Text_Head
+
+from adet.data.dataset_mapper import DatasetMapperWithBasis
+from adet.data.fcpose_dataset_mapper import FCPoseDatasetMapper
+from adet.config import get_cfg
+from adet.checkpoint import AdetCheckpointer
+from adet.evaluation import TextEvaluator
+import numpy as np
+import cv2
+
+import torch.distributed as dist
+dist.init_process_group('gloo', init_method='file:///tmp/somefile', rank=0, world_size=1)
+
+def backward_hook(module, grad_in, grad_out):
+    grad_block.append(grad_out[0].detach())
+
+
+def farward_hook(module, input, output):
+    fmap_block.append(output)
+
+def show_cam_on_image(img, mask, out_dir, index):
+    heatmap = cv2.applyColorMap(np.uint8(255*mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam = heatmap + np.float32(img)
+    cam = cam / np.max(cam)
+
+    path_cam_img = os.path.join(out_dir, "cam_" + str(index) + ".jpg")
+    path_raw_img = os.path.join(out_dir, "raw.jpg")
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    cv2.imwrite(path_cam_img, np.uint8(255 * cam))
+    cv2.imwrite(path_raw_img, np.uint8(255 * img))
+
+def gen_cam(feature_map, grads):
+    """
+    依据梯度和特征图，生成cam
+    :param feature_map: np.array， in [C, H, W]
+    :param grads: np.array， in [C, H, W]
+    :return: np.array, [H, W]
+    """
+    cam = np.zeros(feature_map.shape[1:], dtype=np.float32)  # cam shape (H, W)
+
+    weights = np.mean(grads, axis=(1, 2))  #
+
+    for i, w in enumerate(weights):
+        cam += w * feature_map[i, :, :]
+
+    cam = np.maximum(cam, 0)
+    # cam = cv2.resize(cam, (32, 32))
+    cam = cv2.resize(cam, (255, 255))
+    cam -= np.min(cam)
+    cam /= np.max(cam)
+
+    return cam
+
+class Trainer(DefaultTrainer):
+    """
+    This is the same Trainer except that we rewrite the
+    `build_train_loader`/`resume_or_load` method.
+    """
+    def build_hooks(self):
+        """
+        Replace `DetectionCheckpointer` with `AdetCheckpointer`.
+
+        Build a list of default hooks, including timing, evaluation,
+        checkpointing, lr scheduling, precise BN, writing events.
+        """
+        ret = super().build_hooks()
+        for i in range(len(ret)):
+            if isinstance(ret[i], hooks.PeriodicCheckpointer):
+                self.checkpointer = AdetCheckpointer(
+                    self.model,
+                    self.cfg.OUTPUT_DIR,
+                    optimizer=self.optimizer,
+                    scheduler=self.scheduler,
+                )
+                ret[i] = hooks.PeriodicCheckpointer(self.checkpointer, self.cfg.SOLVER.CHECKPOINT_PERIOD)
+        return ret
+    
+    def resume_or_load(self, resume=True):
+        checkpoint = self.checkpointer.resume_or_load(self.cfg.MODEL.WEIGHTS, resume=resume)
+        if resume and self.checkpointer.has_checkpoint():
+            self.start_iter = checkpoint.get("iteration", -1) + 1
+
+    def train_loop(self, start_iter: int, max_iter: int):
+        """
+        Args:
+            start_iter, max_iter (int): See docs above
+        """
+        logger = logging.getLogger("adet.trainer")
+        logger.info("Starting training from iteration {}".format(start_iter))
+
+        self.iter = self.start_iter = start_iter
+        self.max_iter = max_iter
+
+        #————————————————————添加hooker
+        self.model.proposal_generator.fcos_head.cls_logits.register_forward_hook(farward_hook)
+        self.model.proposal_generator.fcos_head.cls_logits.register_forward_hook(backward_hook)
+        self.model.proposal_generator.fcos_head.cbam.register_forward_hook(backward_hook)
+        self.model.proposal_generator.fcos_head.cbam_cls_logits.register_forward_hook(backward_hook)
+
+        with EventStorage(start_iter) as self.storage:
+            self.before_train()
+            for self.iter in range(start_iter, max_iter):
+                self.before_step()
+                self.run_step()
+
+                ###########################
+                output_dir = self.cfg.OUTPUT_DIR
+                #生成cam
+                # grads_val = grad_block[0].cpu().data.numpy().squeeze()
+                # fmap = fmap_block[0].cpu().data.numpy().squeeze()
+                for i, grads_val in enumerate(grad_block):
+                    grads_val = grads_val.cpu().data.numpy()
+                    fmap = fmap_block[i].cpu().data.numpy().squeeze(0)
+                    cam = gen_cam(fmap, grads_val)
+                    path_img = self._trainer.ima_to_show_CAM[0]['file_name']
+                    img = cv2.imread(path_img, 1)
+                    img_show = np.float32(cv2.resize(img, (255, 255))) / 255
+                    show_cam_on_image(img_show, cam, output_dir, i)
+
+                with open(output_dir + "/which_pic.txt", "a+") as f:
+                    f.write(path_img + '\n')
+                exit()
+                ###########################
+
+                self.after_step()
+            self.after_train()
+
+    def train(self):
+        """
+        Run training.
+
+        Returns:
+            OrderedDict of results, if evaluation is enabled. Otherwise None.
+        """
+        self.train_loop(self.start_iter, self.max_iter)
+        if hasattr(self, "_last_eval_results") and comm.is_main_process():
+            verify_results(self.cfg, self._last_eval_results)
+            return self._last_eval_results
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        """
+        Returns:
+            iterable
+
+        It calls :func:`detectron2.datas.build_detection_train_loader` with a customized
+        DatasetMapper, which adds categorical ori_annotation_file_list as a semantic mask.
+        """
+        if cfg.MODEL.FCPOSE_ON:
+            mapper = FCPoseDatasetMapper(cfg, True)
+        else:
+            mapper = DatasetMapperWithBasis(cfg, True)
+        return build_detection_train_loader(cfg, mapper=mapper)
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        """
+        Create evaluator(s) for a given dataset.
+        This uses the special metadata "evaluator_type" associated with each builtin dataset.
+        For your own dataset, you can simply create an evaluator manually in your
+        script and do not have to worry about the hacky if-else logic here.
+        """
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        evaluator_list = []
+        evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
+        if evaluator_type in ["sem_seg", "coco_panoptic_seg"]:
+            evaluator_list.append(
+                SemSegEvaluator(
+                    dataset_name,
+                    distributed=True,
+                    num_classes=cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES,
+                    ignore_label=cfg.MODEL.SEM_SEG_HEAD.IGNORE_VALUE,
+                    output_dir=output_folder,
+                )
+            )
+        if evaluator_type in ["coco", "coco_panoptic_seg"]:
+            evaluator_list.append(COCOEvaluator(dataset_name, cfg, True, output_folder))
+        if evaluator_type == "coco_panoptic_seg":
+            evaluator_list.append(COCOPanopticEvaluator(dataset_name, output_folder))
+        if evaluator_type == "pascal_voc":
+            return PascalVOCDetectionEvaluator(dataset_name)
+        if evaluator_type == "lvis":
+            return LVISEvaluator(dataset_name, cfg, True, output_folder)
+        if evaluator_type == "text":
+            return TextEvaluator(dataset_name, cfg, True, output_folder)
+        if len(evaluator_list) == 0:
+            raise NotImplementedError(
+                "no Evaluator for the dataset {} with the type {}".format(
+                    dataset_name, evaluator_type
+                )
+            )
+        if len(evaluator_list) == 1:
+            return evaluator_list[0]
+        return DatasetEvaluators(evaluator_list)
+
+    @classmethod
+    def test_with_TTA(cls, cfg, model):
+        logger = logging.getLogger("adet.trainer")
+        # In the end of training, run an evaluation with TTA
+        # Only support some R-CNN models.
+        logger.info("Running inference with test-time augmentation ...")
+        model = GeneralizedRCNNWithTTA(cfg, model)
+        evaluators = [
+            cls.build_evaluator(
+                cfg, name, output_folder=os.path.join(cfg.OUTPUT_DIR, "inference_TTA")
+            )
+            for name in cfg.DATASETS.TEST
+        ]
+        res = cls.test(cfg, model, evaluators)
+        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
+        return res
+
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    # cfg.set_new_allowed(is_new_allowed=True)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+
+    rank = comm.get_rank()
+    setup_logger(cfg.OUTPUT_DIR, distributed_rank=rank, name="adet")
+
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        AdetCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model) # d2 defaults.py
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        return res
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop or subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    if cfg.TEST.AUG.ENABLED:
+        trainer.register_hooks(
+            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
+        )
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    fmap_block = list()
+    grad_block = list()
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
