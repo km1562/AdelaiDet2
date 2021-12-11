726c8a00f68ef56ef303f7a8546f10269e7ffc1c
km1562 <wkm690772123@gmail.com>
2021-12-11 16:39:56 +0800

    "生成polygen"

--- AdelaiDet/adet/modeling/batext/compute_bezier_iou.py
+++ AdelaiDet/adet/modeling/batext/compute_bezier_iou.py
@@ -1,10 +1,14 @@
+import torch
+from shapely.geometry import Polygon
 from pyxllib.algo.geo import ComputeIou
 
 def compute_bezier_iou(bezier_pred, bezier_targets):
     beziers_iou = []
     bezier_nums = len(bezier_pred)
     for i in range(bezier_nums):
-        bezier_iou = ComputeIou.polygon2(bezier_pred[i], bezier_targets[i])
+        pts1 = Polygon(bezier_pred[i])
+        pts2 = Polygon(bezier_targets[i])
+        bezier_iou = ComputeIou.polygon(pts1, pts2)
         beziers_iou.append(bezier_iou)
 
     return beziers_iou
