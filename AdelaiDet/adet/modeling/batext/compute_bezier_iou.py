d6c0fc7a27275d62579647483da463200bbc564a
km1562 <wkm690772123@gmail.com>
2021-12-11 16:44:01 +0800

    "成功"

--- AdelaiDet/adet/modeling/batext/compute_bezier_iou.py
+++ AdelaiDet/adet/modeling/batext/compute_bezier_iou.py
@@ -6,8 +6,8 @@ def compute_bezier_iou(bezier_pred, bezier_targets):
     beziers_iou = []
     bezier_nums = len(bezier_pred)
     for i in range(bezier_nums):
-        pts1 = Polygon(bezier_pred[i])
-        pts2 = Polygon(bezier_targets[i])
+        pts1 = Polygon(bezier_pred[i].reshape(-1, 2))
+        pts2 = Polygon(bezier_targets[i].reshape(-1, 2))
         bezier_iou = ComputeIou.polygon(pts1, pts2)
         beziers_iou.append(bezier_iou)
 
