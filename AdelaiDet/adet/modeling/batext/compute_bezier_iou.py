aabcf4363209ddc1ceb0e2d22659afd0702772da
km1562 <wkm690772123@gmail.com>
2021-12-12 14:26:54 +0800

    "先生成beizer曲线，再计算iou才对"

--- AdelaiDet/adet/modeling/batext/compute_bezier_iou.py
+++ AdelaiDet/adet/modeling/batext/compute_bezier_iou.py
@@ -1,13 +1,29 @@
 import torch
 from shapely.geometry import Polygon
 from pyxllib.algo.geo import ComputeIou
+import numpy as np
+
+def bezier_to_poly(bezier):
+    # bezier to polygon
+    u = np.linspace(0, 1, 20)
+    bezier = np.array(bezier)
+    bezier = bezier.reshape(2, 4, 2).transpose(0, 2, 1).reshape(4, 4)
+    points = np.outer((1 - u) ** 3, bezier[:, 0]) \
+             + np.outer(3 * u * ((1 - u) ** 2), bezier[:, 1]) \
+             + np.outer(3 * (u ** 2) * (1 - u), bezier[:, 2]) \
+             + np.outer(u ** 3, bezier[:, 3])
+    points = np.concatenate((points[:, :2], points[:, 2:]), axis=None)
+    points = points.reshape(-1).tolist()
+    return points
 
 def compute_bezier_iou(bezier_pred, bezier_targets):
     beziers_iou = []
     bezier_nums = len(bezier_pred)
     for i in range(bezier_nums):
-        pts1 = Polygon(bezier_pred[i].reshape(-1, 2))
-        pts2 = Polygon(bezier_targets[i].reshape(-1, 2))
+        pts1 = bezier_to_poly(bezier_pred[i])
+        pts2 = bezier_to_poly(bezier_targets[i])
+        pts1 = Polygon(pts1)
+        pts2 = Polygon(pts1)
         bezier_iou = ComputeIou.polygon(pts1, pts2)
         beziers_iou.append(bezier_iou)
 
